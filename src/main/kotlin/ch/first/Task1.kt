e954e20a90a9eaded7ba9190bafbfa2ae8c9db9e
17773791 <Sudarev.A.A@sberbank.ru>
2022-12-27 17:31:41 +0300

    initial commit

---
+++ src/main/kotlin/ch/first/Task1.kt
@@ -0,0 +1,12 @@
+package ch.first
+
+fun main(args: Array<String>) {
+    var x = 1
+    while(x < 3) {
+        print(if (x == 1) "Yab" else "Dab")
+        print("ba")
+        x++
+    }
+    if(x == 3)
+        print("Do")
+}
